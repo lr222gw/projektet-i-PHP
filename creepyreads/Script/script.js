ac47b72e2869e8b5c6b4fd541143a3209ff64886
lr222gw <lr222gw@student.lnu.se>
2014-10-26 21:23:47 +0100

    läsvy är schysterbar

--- creepyreads/Script/script.js
+++ creepyreads/Script/script.js
@@ -7,6 +7,116 @@ function toRun(){
     reziseMusicPlayer();
     allowCookies();
     didUserActivateVoteSlide();
+    userChangeStoryView();
+}
+function userChangeStoryView(){
+/*    if(localStorage["controllStory"] === undefined){
+        localStorage["controllStory"] = [];
+    }*/
+    var storyToChange = document.getElementsByClassName("story")[0];
+    var insideStory = document.getElementsByClassName("listStoryColumn")[0];
+
+    var divForController = document.createElement("DIV");
+    divForController.id = "controllStory"
+    insideStory.insertBefore(divForController, storyToChange);
+    //Nu har vi placerat ut diven där vi ska placera våra kontrollrar...
+
+    if(localStorage["textSize"] == undefined){
+        localStorage["textSize"] = 15;
+        storyToChange.style.fontSize = localStorage["textSize"]+"px";
+    }else{
+        storyToChange.style.fontSize = localStorage["textSize"]+"px";
+    }
+
+    if(localStorage["lineHeight"] == undefined){
+        localStorage["lineHeight"] = 20;
+        storyToChange.style.lineHeight = localStorage["lineHeight"]+"px";
+    }else{
+        storyToChange.style.lineHeight = localStorage["lineHeight"]+"px";
+    }
+
+    if(localStorage["DayOrNightMode"] == undefined){
+        localStorage["DayOrNightMode"] = "day";
+    }
+
+    var pageDimmer = document.createElement("DIV");
+    pageDimmer.id = "page-cover";
+    var dimButton = document.createElement("BUTTON");
+    dimButton.innerHTML = "Dim site";
+    dimButton.onclick = function(){
+        if(pageDimmer.style.display != "block"){
+            var corner = document.getElementById("body");
+            pageDimmer.style.display = "block";
+            corner.appendChild(pageDimmer);
+            insideStory.style.zIndex = "9999";
+            insideStory.style.position = "absolute";
+        }else{
+            pageDimmer.onclick();
+        }
+    }
+    pageDimmer.onclick = function(){
+        pageDimmer.style.display = "none";
+        insideStory.style.zIndex = "100";
+        insideStory.style.position = "static";
+    }
+
+    var DayOrNightMode = document.createElement("BUTTON");
+    DayOrNightMode.innerHTML = "Day or Night mode";
+    DayOrNightMode.onclick = function(){
+        if(localStorage["DayOrNightMode"] === "night"){
+            localStorage["DayOrNightMode"] = "day";
+            storyToChange.style.backgroundColor = "rgb(229, 229, 229)";
+            storyToChange.style.color = "Black";
+        }else{
+            localStorage["DayOrNightMode"] = "night";
+            storyToChange.style.backgroundColor = "Black";
+            storyToChange.style.color = "rgb(221, 221, 221)";
+
+        }
+
+    }
+
+
+    var bigLineHeightButton = document.createElement("BUTTON");
+    bigLineHeightButton.innerHTML = "Bigger Line Height";
+    bigLineHeightButton.onclick = function(){
+        localStorage["lineHeight"] = parseFloat(localStorage["lineHeight"]) +0.5;
+        storyToChange.style.lineHeight = localStorage["lineHeight"]+"px";
+    }
+
+    var SmallLineHeightButton = document.createElement("BUTTON");
+    SmallLineHeightButton.innerHTML = "Smaller Line Height";
+    SmallLineHeightButton.onclick = function(){
+        if(parseInt(localStorage["lineHeight"]) > 15 ){
+            localStorage["lineHeight"] = parseFloat(localStorage["lineHeight"]) -0.5;
+             storyToChange.style.lineHeight = localStorage["lineHeight"]+"px";
+        }
+    }
+
+    var bigTextButton = document.createElement("BUTTON");
+    bigTextButton.innerHTML = "Bigger Text";
+    bigTextButton.onclick = function(){
+        localStorage["textSize"] = parseInt(localStorage["textSize"]) +1;
+        storyToChange.style.fontSize = localStorage["textSize"]+"px";
+    }
+
+    var SmallTextButton = document.createElement("BUTTON");
+    SmallTextButton.innerHTML = "Smaller Text";
+    SmallTextButton.onclick = function(){
+        if(parseInt(localStorage["textSize"]) > 5 ){
+            localStorage["textSize"] = parseInt(localStorage["textSize"]) -1;
+            storyToChange.style.fontSize = localStorage["textSize"]+"px";
+        }
+    }
+
+    divForController.appendChild(bigTextButton);
+    divForController.appendChild(SmallTextButton);
+    divForController.appendChild(bigLineHeightButton);
+    divForController.appendChild(SmallLineHeightButton);
+    divForController.appendChild(DayOrNightMode);
+    divForController.appendChild(dimButton);
+
+
 }
 
 function didUserActivateVoteSlide(){
